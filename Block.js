99da321d9b8b3b802f53cabe5a367672812a1a71
Poojap19create <74517001+Poojap19create@users.noreply.github.com>
2022-05-02 21:11:20 +0530

    Update Block.js

--- Block.js
+++ Block.js
@@ -8,7 +8,10 @@ class Block{
         this.body = Bodies.rectangle(x, y, width, height, options);
         this.width = width;
         this.height = height;
-        this.image= loadImage("block.png")
+        
+
+        //Add code for loading the image block.png        
+        
         World.add(world, this.body);
         
       }
@@ -21,12 +24,10 @@ class Block{
           image(this.image,pos.x,pos.y,this.width, this.height);
         }
       else{
-        World.remove(world, this.body);
-        push();
-        this.visiblity = this.visiblity - 5;
-        tint(255,this.visiblity);
-        image(this.image, this.body.position.x, this.body.position.y,this.width, this.height);
-        pop();
+        
+          /// Write the code for push and pop 
+          
+          
       }
     }
 }
